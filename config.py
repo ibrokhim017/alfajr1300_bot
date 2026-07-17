0753d1943f66b8d27e72f456e6887ca3f3d6dc18
ibrokhim017 <akhmedov.smm017@gmail.com>
2026-07-17 13:36:24 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("8787073654:AAFfsdFWiQiwmf9JFRU_SiajGpoC8lvXh5o")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 GROUP_ID = int(os.getenv("GROUP_ID", "-1004235445982"))
 DB_NAME = "database.db"
